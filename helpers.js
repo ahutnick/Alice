8622c2c2f1ac608bd05f246883ee498740fcbff7
Tasha Hutnick <ahutnick@sas.upenn.edu>
2024-09-29 20:16:26 -0400

    mockup first commit

---
+++ helpers.js
@@ -0,0 +1,23 @@
+function choices(choices) {
+    var line = "";
+    var index = 0;
+    var routes = [];
+    choices.forEach(choice => {
+        line += `<button id="choice${index}"">${choice.text}</button>`
+        routes.push(choice.route)
+        index++;
+    });
+
+    $("#Choices").html(line);
+    routes.forEach((route, index) => {
+        $(`#choice${index}`).click(() => updateChapter(route));
+    })
+}
+
+export function updateChapter(place) {
+    $.getJSON("./chapters.json", (data) => {
+        var choiceList = data.seven.mainText[place].choices || null;
+        $("#MainText").html(data.seven.mainText[place].text);
+        choiceList ? choices(data.seven.mainText[place].choices) : $("#Choices").empty();
+    })
+};
